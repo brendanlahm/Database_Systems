9859227437a1b90196daed3952ad63ac4ee2bb77
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2026-01-29 21:05:23 +0100

    Added Exercise 11

---
+++ Bikestore Sample DB/Exercise11.sql
@@ -0,0 +1,21 @@
+﻿-- Exercise 11
+USE BikeStore
+
+-- Task 1: Division
+-- a) Re-order products not present in every store	
+select product_name, stocks.store_id
+from products, stores, stocks
+
+except -- Return every row in A that does not appear verbatim in B
+	
+select product_name, stocks.store_id
+from products join stocks on(stocks.product_id=products.product_id)
+where stocks.quantity >0
+
+
+
+
+
+
+
+
