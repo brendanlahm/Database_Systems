2ec456800f8ce054d7514b1340f9d7296c0d35ef
brendanlahm <65281324+brendanlahm@users.noreply.github.com>
2026-01-30 10:35:04 +0100

    Added Exercise 11: Task 1: part b

--- Bikestore Sample DB/Exercise11.sql
+++ Bikestore Sample DB/Exercise11.sql
@@ -10,9 +10,26 @@ except -- Return every row in A that does not appear verbatim in B
 	
 select product_name, stocks.store_id
 from products join stocks on(stocks.product_id=products.product_id)
-where stocks.quantity >0
+where stocks.quantity>0 -- Only an item w/ quantity 0 in some store will end up in list A
 
+-- b) List of products in stock in every store
+select product_name
+from products
 
+except -- Only items w/ quantity >0 in all stores will end up in list A
+
+select product_name
+from (
+	select product_name, store_id
+	from products, stores
+
+	except
+	
+	select product_name, stocks.store_id
+	from products join stocks on(stocks.product_id=products.product_id)
+	where stocks.quantity>0
+) as p
+order by product_name
 
 
 
